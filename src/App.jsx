84911097578f32e5edbb6132021f2cd865057ba2
L-David-26 <davidlez2mgk@gmail.com>
2021-10-09 14:07:34 -0500

    initial app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+
 import './App.css'
+import Reduccer from './Components/Reduccer'
+
 function App() {
   return (
     <div className="App">
+      <h1>
+        hola mundo con un aplicacion de react y con vite
+      </h1>
+      <Reduccer />
     </div>
   )
 }
 
 export default App
-
---
+++ src/Components/Reduccer.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+const Reduccer = () => {
+  return (
+    <div className="reduccer__app">
+      <h1>hola todo dios xdxd</h1>
+    </div>
+  );
+};
+
+
+export default Reduccer
