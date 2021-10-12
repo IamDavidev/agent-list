7ccc7106e212a1acb2d3430f0f9a288a0f52136b
L-David-26 <davidlez2mgk@gmail.com>
2021-10-12 11:20:57 -0500

    add porbablity of form

--- src/Components/FormContacts/index.jsx
+++ src/Components/FormContacts/index.jsx
@@ -1,32 +1,60 @@
+import { useState } from 'react';
 import './index.css';
 
 const FormContacts = (props) => {
   const { dispatch } = props;
-  const objNew = {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleNumber = (e)=>{
+    setNumber(e.target.value);
+  }
+
+  const newContact = {
     type: 'addContact',
     payload: {
-      id: '123',
-      name: 'davidpayload',
-      number: 7711093966,
+      id: 72834982,
+      name: name,
+      number: number,
     },
   };
+
   const handleAdd = (evt) => {
     evt.preventDefault();
-    dispatch(objNew);
-    
+    -dispatch(newContact);
+    console.log(newContact);
   };
   return (
     <div className="form__contacts">
       <h2 className="title__contacts">contacts xd</h2>
-      <form onSubmit={handleAdd}>
+      <form onSubmit={handleAdd} autoComplete="off">
         <label>
           Name : <br />
-          <input type="text" placeholder="Add name" autoFocus />
+          <input
+            name="number"
+            onChange={handleName}
+            value={name}
+            type="text"
+            placeholder="Add name"
+            autoFocus
+            required
+          />
         </label>
         <br />
         <label>
           Number: <br />
-          <input type="text" placeholder="add Number" autoFocus />
+          <input
+            type="text"
+            onChange={handleNumber}
+            name="number"
+            value={number}
+            placeholder="add Number"
+            autoFocus
+            required
+          />
         </label>
         <br />
         <button className="form__button">add contact</button>
